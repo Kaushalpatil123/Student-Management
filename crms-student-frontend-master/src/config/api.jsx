1a82839528c791e7be1116307270126698775a6c
kaushal patil <91415972+Kaushalpatil123@users.noreply.github.com>
2025-08-30 10:30:57 +0530

    Update api.jsx

--- crms-student-frontend-master/src/config/api.jsx
+++ crms-student-frontend-master/src/config/api.jsx
@@ -1,3 +1,3 @@
 // export const Backend_URL = "http://localhost:5000/students"
-export const Backend_URL = "https://school-student-management-backend.onrender.com"
+export const Backend_URL = "https://school-student-management-backend.onrender.com/students"
 // export const Backend_URL= import.meta.env.VITE_BACKEND_URL
